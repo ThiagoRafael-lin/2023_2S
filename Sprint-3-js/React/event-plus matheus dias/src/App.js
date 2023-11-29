15a32d7f1d1a2f2443f98f302ada18ffbae4aba1
Thiago Domingues Rafael <47195701867@info.local>
2023-11-29 17:14:44 -0300

    Att

---
+++ Sprint-3-js/React/event-plus matheus dias/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Rotas from './routes';
+import { UserContext } from './context/AuthContext';
+import { useState } from 'react';
+
+function App() {
+
+const [userData, setUserData] = useState({})
+
+  return (
+
+    <UserContext.Provider value={{userData, setUserData}}>
+
+      <Rotas/>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
